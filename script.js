b6d198291bb5a48d20c9f6e068556e387342395c
Thilak Jay <thilakshanj@gmail.com>
2022-02-03 22:36:47 -0500

    polished up a bit, modified the functions, added size and clear buttons.

--- script.js
+++ script.js
@@ -1,47 +1,50 @@
-let container = document.querySelector("#container");
-let clearButton = document.querySelector("#clearBtn");
+const container = document.querySelector("#container");
+const sizeButton = document.querySelector("#sizeBtn");
+const clearButton = document.querySelector("#clearBtn");
 const containerWidth = container.clientWidth;
 const containerHeight = container.clientHeight;
 
 clearButton.addEventListener("click", clearGrid);
+sizeButton.addEventListener("click", fillGrid);
 
-
-function fillGrid(size) {
-    //1.create row(div) 
-    //2.create squares to fill out the row, then append them to row
-    //3.now append row to container
-    //repeat steps 1-3 until container is filled out
-
-    for(let i=1; i<=size; i++) { //add row by one by one
-        let row = document.createElement("div");
-        row.className = "row";
-        for(let j=1; j<=size; j++){ //add squares one by one to each row
-            let square = document.createElement("div");        
-            square.className = "square";
-            square.style.width = (containerWidth/size) + "px";
-            square.style.height = (containerHeight/size) + "px";
-            square.style.filter = "brightness(1)";
-            square.addEventListener("mouseover", () => {
-                    addRandomRGB(square);        
-                    darken(square);          
-                });
-            row.appendChild(square);
-        }
-        container.appendChild(row);           
-    }       
-}
-
-function clearGrid() {
+function fillGrid() {
     while(container.firstChild) {
         container.removeChild(container.firstChild);
-    }  
-    let size = prompt("Enter grid size (1 - 100)")
+    }
+
+    let size = prompt("Enter grid size (1 - 100)");
+
     if(size === null || size === "") {
         return;
     }else if (size <= 0 || size > 100){
-        clearGrid();
-    }else {
-        fillGrid(size);
+        fillGrid();
+    }else if (size > 0 && size <= 100){
+        for(let i=1; i<=size; i++) { //add row by one by one
+            let row = document.createElement("div");
+            row.className = "row";
+            for(let j=1; j<=size; j++){ //add squares one by one to each row
+                let square = document.createElement("div");        
+                square.className = "square";
+                square.style.width = (containerWidth/size) + "px";
+                square.style.height = (containerHeight/size) + "px";
+                square.style.filter = "brightness(1)";
+                square.addEventListener("mouseover", () => {
+                        addRandomRGB(square);        
+                        darken(square);          
+                    });
+                row.appendChild(square);
+            }
+            container.appendChild(row);        
+        } 
+    }              
+}
+
+function clearGrid() {
+    let squares = document.querySelectorAll(".square");
+    
+    for(let node of squares) {
+        node.style.backgroundColor = "";
+        node.style.filter = "brightness(1)";
     }
 }
 
@@ -61,7 +64,3 @@ function darken(node) {
         node.style.filter = `brightness(${brightValue})`;
     }
 }
-
-
-//fitting up to 100 x 100 squares without modifying size of grid.
-//taking user input for up to 100 x 100 sized grid.
