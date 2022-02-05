910ef8439af4ec91a4846a9b409bd973fc38b61a
Thilak Jay <thilakshanj@gmail.com>
2022-02-04 21:59:07 -0500

    added UI, more functionality

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # etch-a-sketch
+
+This file creates a grid of size X by X, where is X is inputted by the user. Then whenever a mouse hovers over any of the squares, a random color is assigned to that square. With each subsequent hover over a colored square, that square turns 10% darker and becomes black after 10 passes. 
+
+Thigns Learned: 
+#1 box-sizing: border box. This allowed the divs to be aligned within the borders of the container without having to calculate for padding/margin of each square. 
+
+#2 filter: brightness(x). This is what makes backgrounds/colors/images turn darker or brighter. I used this property and function to make squares turn 'blacker' with each hover.
+
+#3 DOM Manipulation. Extra practice for creating and manipulating elements with javascript. 
+
+#4 Clearing out all the children within a parent element. The two lines of code within the fillGrid() function showcase this effectively. A great way for clearing and emptying out a parent element.
--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 const container = document.querySelector("#container");
 const sizeButton = document.querySelector("#sizeBtn");
 const clearButton = document.querySelector("#clearBtn");
+const eraseButton = document.querySelector("#eraseBtn");
+const TOTAL_DARK = "brightness(0)";
+const STANDARD_BRIGHT = "brightness(1)";
+
+let drawMode = true;
+let eraseMode = false;
+
 const containerWidth = container.clientWidth;
 const containerHeight = container.clientHeight;
 
 clearButton.addEventListener("click", clearGrid);
-sizeButton.addEventListener("click", fillGrid);
+sizeButton.addEventListener("click", () => {
+    let size = prompt("Enter grid size (1 - 100)"); 
+    fillGrid(size);
+});
+eraseButton.addEventListener("click", () => {
+    if(drawMode == true) {
+        eraseMode = true;
+        drawMode = false;
+        eraseButton.value = "Draw";
+    }else{
+        eraseMode = false;
+        drawMode = true;
+        eraseButton.value = "Erase";
+    }
+});
 
-function fillGrid() {
+fillGrid(15);
+
+function fillGrid(size) {
     while(container.firstChild) {
         container.removeChild(container.firstChild);
     }
-
-    let size = prompt("Enter grid size (1 - 100)");
 
     if(size === null || size === "") {
         return;
@@ -23,15 +44,12 @@ function fillGrid() {
             let row = document.createElement("div");
             row.className = "row";
             for(let j=1; j<=size; j++){ //add squares one by one to each row
-                let square = document.createElement("div");        
+                let square = document.createElement("div");
                 square.className = "square";
                 square.style.width = (containerWidth/size) + "px";
                 square.style.height = (containerHeight/size) + "px";
                 square.style.filter = "brightness(1)";
-                square.addEventListener("mouseover", () => {
-                        addRandomRGB(square);        
-                        darken(square);          
-                    });
+                square.addEventListener("mouseover", draw);                                       
                 row.appendChild(square);
             }
             container.appendChild(row);        
@@ -40,27 +58,39 @@ function fillGrid() {
 }
 
 function clearGrid() {
-    let squares = document.querySelectorAll(".square");
-    
+    let squares = document.querySelectorAll(".square");   
     for(let node of squares) {
         node.style.backgroundColor = "";
         node.style.filter = "brightness(1)";
     }
 }
 
-function addRandomRGB(node) {
-    if(!node.style.backgroundColor){
-        let r = Math.floor(Math.random() * 256);
-        let g = Math.floor(Math.random() * 256);
-        let b = Math.floor(Math.random() * 256);
-        node.style.backgroundColor = `rgb(${r},${g},${b})`;
+function draw() {
+    if(drawMode) {
+        addRandomRBG(this);
+        darken(this);
+    }else if(eraseMode) {
+        erase(this);
     }
 }
 
+function addRandomRBG(node) {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    node.style.backgroundColor = `rgb(${r},${g},${b})`;
+}
+
 function darken(node) {
-    if(node.style.filter !== "brightness(0)") {
+    if(node.style.filter !== TOTAL_DARK) {
         let brightValue = parseFloat(node.style.filter.slice(11,-1));
         brightValue -= 0.1;
         node.style.filter = `brightness(${brightValue})`;
     }
 }
+
+function erase(node){
+    node.style.backgroundColor = "white";
+    node.style.filter = STANDARD_BRIGHT;
+}
+
